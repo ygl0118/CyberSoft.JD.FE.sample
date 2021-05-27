b65fda9e5c257f46572517ffc7ad3a1bbc43f3a8
UJ0 <urenyou@cybersoft4u.com>
2021-05-27 18:12:44 +0800

    Feature: [HelloWorld] add style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,26 @@
     <p>
       All you need to do is to try to finish following statements:
     </p>
-    <ol>
-      <li>Add some buttons that you can switch to other pages with at least two different contents</li>
-      <li>Create new components for functional interactions, like exchange rate conversion between NTD and others</li>
-      <li>What ever you want do to get more bonus</li>
-    </ol>
+    <div class="content-wrapper">
+      <div>
+        <input type="checkbox" id="todo1" name="todo1" value="todo1">
+        <label for="todo1">Add some buttons that you can switch to other pages with at least two different contents</label>
+      </div>
+      <div>
+        <input type="checkbox" id="todo2" name="todo2" value="todo2">
+        <label for="todo2">Create new components for functional interactions, like exchange rate conversion between NTD and others</label>
+      </div>
+      <div>
+        <input type="checkbox" id="todo3" name="todo3" value="todo3">
+        <label for="todo3">What ever you want do to get more bonus</label>
+      </div>
+    </div>
+    <h2>
+      Reference
+    </h2>
+    <p>
+      臺灣期貨交易所 OAS <a href="https://openapi.taifex.com.tw/" target="_blank">https://openapi.taifex.com.tw/</a>
+    </p>
   </div>
 </template>
 
@@ -23,5 +38,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
+.content-wrapper {
+  display: flex;
+  justify-content: center;
 
+  div {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
 </style>
