2526befd6c6714b890b9e5f9c2fab6521a31713c
UJ0 <urenyou@cybersoft4u.com>
2021-05-27 16:45:24 +0800

    Feature: [Home] update contents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Coding Challenge"/>
   </div>
 </template>
 
