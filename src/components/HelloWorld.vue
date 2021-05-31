58741af70503b38e44e726df4851141eab8e009b
UJ0 <urenyou@cybersoft4u.com>
2021-05-31 11:38:50 +0800

    Adjust: [HelloWorld] update documentation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,20 +7,26 @@
     <div class="content-wrapper">
       <div>
         <input type="checkbox" id="todo1" name="todo1" value="todo1">
-        <label for="todo1">Add some buttons that you can switch to other pages with at least two different contents</label>
+        <label for="todo1">Add some buttons anywhere that you can switch to other pages with at least two different contents</label>
       </div>
       <div>
         <input type="checkbox" id="todo2" name="todo2" value="todo2">
-        <label for="todo2">Create new components for functional interactions, like exchange rate conversion between NTD and others</label>
+        <label for="todo2">Create new components for functional interactivities, like exchange rate conversion between NTD and others</label>
       </div>
       <div>
         <input type="checkbox" id="todo3" name="todo3" value="todo3">
-        <label for="todo3">What ever you want do to get more bonus</label>
+        <label for="todo3">What ever you want do to get more bonus, like beautifying components, more functional interactivities, etc.</label>
       </div>
     </div>
     <h2>
       Reference
     </h2>
+    <p>
+      Vue.js <a href="https://v3.vuejs.org/guide/introduction.html" target="_blank">https://v3.vuejs.org/guide/introduction.html</a>
+    </p>
+    <p>
+      vue-axios - npm <a href="https://www.npmjs.com/package/vue-axios" target="_blank">https://www.npmjs.com/package/vue-axios</a>
+    </p>
     <p>
       臺灣期貨交易所 OAS <a href="https://openapi.taifex.com.tw/" target="_blank">https://openapi.taifex.com.tw/</a>
     </p>
